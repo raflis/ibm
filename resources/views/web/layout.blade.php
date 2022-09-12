1d95c41bf4525fbfe56b6b67a77c12a2a0197232
Raflis <raflisd@gmail.com>
2022-09-12 18:24:17 -0500

    cambio de titulo

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>IBM Cloud Rocks</title>
+    <title>IBM Innovation Summit</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="routeName" content="{{ Route::currentRouteName() }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
